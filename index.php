57efecaa641f1d3c95575dddde4e89cee05232b4
ryrua14 <ryru136@gmail.com>
2020-03-26 23:19:30 -0400

    Create form

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <title>TO-DO List</title>
+    <!--
+    Todo list that saves tasks that a user needs to do
+    Author: Ryan Rua
+    -->
+    
+    <!--Stylesheets-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    </head>
+    <body>
+        <div class="container">
+            <form>
+                <div class="form-group">
+                    <label for="taskInput">Enter the task</label>
+                    <input type="text" class="form-control" id="taskInput" placeholder="Enter Task">
+                </div>
+                <button type="submit" class="btn btn-primary">Add Task</button>
+            </form>
+        </div>
+    </body>
+</html>
